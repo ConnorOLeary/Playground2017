09e96dae27caccdce9c33236d70ee44d572fdb92
ConnorOLeary <themidlvlboss@gmail.com>
2017-09-25 08:53:41 -0600

    Made the new playground and did 3 of the assignment requirements

--- secondPlayground.playground/Contents.swift
+++ secondPlayground.playground/Contents.swift
@@ -1,5 +1,32 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//Explicit type declartion with no value
+var ramen :String
+//Implicit Type declaration with a vlue to assign type
+var soup = "Ramen"
+ramen = soup
+print(ramen)
+print(soup)
+//ramen = 7
+var myAge :Int
+myAge = -420
+print(myAge)
+myAge = myAge + 54322456
+var someValue = 3.1415 * 56
+someValue = someValue * 231.1
+var isReal :Bool
+isReal = false
+isReal = true
+let myName :String
+myName = "Connor"
+var your :String
+your = "Your "
+var waifu :String
+waifu = "waifu "
+var isTrash :String
+isTrash = "is trash "
+var trueStatement :String
+trueStatement = your + waifu + isTrash
 
-var str = "Hello, playground"
+print(trueStatement)
---
+++ stringPlayground.playground/Contents.swift
@@ -0,0 +1,32 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//StringChange
+var thought :String
+thought = "I'm thinking about stuff right now"
+print(thought)
+thought = "Now I'm thinking about other stuff"
+print(thought)
+
+
+//StringInterpolation
+var your :String
+your = "Your "
+var waifu :String
+waifu = "waifu "
+var isTrash :String
+isTrash = "is trash "
+var trueStatement :String
+trueStatement = your + waifu + isTrash
+print(trueStatement)
+
+
+//CountingCharacters
+var count :String
+count = "I wonder how many characters this string has"
+print(count)
+print(count.characters.count)
+
+
+//ComparingStrings
